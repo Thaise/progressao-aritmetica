f7297382ce698ce0b96d6addabc607378c993c45
Thaise <thaisefloripa@gmail.com>
2011-10-27 16:46:16 -0200

    Calcula o enésimo termo de uma PA

--- test/ProgressaoAritmeticaTest.java
+++ test/ProgressaoAritmeticaTest.java
@@ -7,42 +7,34 @@ public class ProgressaoAritmeticaTest {
 
 	@Test
 	public void deveCalcularTermoNDeUmaProgressaoAritmeticaCrescente() {
-		String[] numerosSeparados = {"1","3","5","7","9"};
+		int[] numerosInteiros = {1,3,5,7,9};
 		int n = 50;
-		String progressaoAritmetica = EncontraPa.calcula(numerosSeparados, n);
-		assertEquals("O 50 termo da P.A. é 99 -> 1,3,5,7,9...99", progressaoAritmetica);
+		int progressaoAritmetica = EncontraPa.calcula(numerosInteiros, n);
+		assertEquals(99, progressaoAritmetica);
 	}
 	
 	@Test
 	public void deveCalcularTermoNDeUmaProgressaoAritmeticaCrescente2() {
-		String[] numerosSeparados = {"2","6","10","14","18"};
+		int[] numerosInteiros = {2,6,10,14,18};
 		int n = 142;
-		String progressaoAritmetica = EncontraPa.calcula(numerosSeparados, n);
-		assertEquals("O 142 termo da P.A. é 566 -> 2,6,10,14,18...566", progressaoAritmetica);
+		int progressaoAritmetica = EncontraPa.calcula(numerosInteiros, n);
+		assertEquals(566, progressaoAritmetica);
 	}
 	
 	@Test
 	public void deveCalcularTermoNDeUmaProgressaoAritmeticaEstacionaria() {
-		String[] numerosSeparados = {"3","3","3","3"};
+		int[] numerosInteiros = {3,3,3,3,3};
 		int n = 142;
-		String progressaoAritmetica = EncontraPa.calcula(numerosSeparados, n);
-		assertEquals("O 142 termo da P.A. é 3 -> 3,3,3,3...3", progressaoAritmetica);
+		int progressaoAritmetica = EncontraPa.calcula(numerosInteiros, n);
+		assertEquals(3, progressaoAritmetica);
 	}
 	
 	@Test
 	public void deveCalcularTermoNDeUmaProgressaoAritmeticaDecrescente() {
-		String[] numerosSeparados = {"5","4","3","2","1"};
+		int[] numerosInteiros = {5,4,3,2,1};
 		int n = 10;
-		String progressaoAritmetica = EncontraPa.calcula(numerosSeparados, n);
-		assertEquals("O 10 termo da P.A. é -4 -> 5,4,3,2,1...-4", progressaoAritmetica);
-	}
-	
-	@Test
-	public void deveRetornarQueEsteConjuntoNaoEUmaPA() {
-		String[] numerosSeparados = {"2","3","4","8","19"};
-		int n = 142;
-		String progressaoAritmetica = EncontraPa.calcula(numerosSeparados, n);
-		assertEquals("Não é uma P.A.!", progressaoAritmetica);
+		int progressaoAritmetica = EncontraPa.calcula(numerosInteiros, n);
+		assertEquals(-4, progressaoAritmetica);
 	}
 	
 }
